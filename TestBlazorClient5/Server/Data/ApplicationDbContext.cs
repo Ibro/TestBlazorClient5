28d67db98b9f9fc34dd143b91bb1461779702f72
Ibro <ibro@ibro.me>
2020-08-13 22:21:11 +0200

    Add project files.

---
+++ TestBlazorClient5/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using TestBlazorClient5.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBlazorClient5.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
